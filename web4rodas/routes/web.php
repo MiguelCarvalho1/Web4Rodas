8aa6b517363592090f8c6f4d958d45321fdae0eb
migue <miguelangelosilvacarvalho96@gmail.com>
2022-05-25 17:01:37 +0100

    Incio da Pagina de Web4Rodas

--- web4rodas/routes/web.php
+++ web4rodas/routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/inicial', function () {
+    return view('incial');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
