f07d87d6beba1496b3e455a366eb217e387a6c9d
migue <miguelangelosilvacarvalho96@gmail.com>
2022-06-02 00:30:58 +0100

    Criação de duas novas tabelas e acrescimo de duas chaves estrangeiras tabela de carro e de motorista para fazer a relação

---
+++ web4rodas/database/migrations/2022_06_01_232904_add_category_to_tipo_veiculo.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+            Schema::table('tipo_veiculo', function (Blueprint $table) {
+    
+                $table->unsignedBigInteger('tipo_id')->unsigned()->nullable();
+    
+                $table->foreign('tipo_id')->references('id')->on('tipo_veiculo')->onDelete('cascade');
+            });
+        }
+    
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::table('tipo_veiculo', function (Blueprint $table) {
+                $table->dropColumn('tipo_id')->unsigned()->nullable();
+            });
+        }
+};
